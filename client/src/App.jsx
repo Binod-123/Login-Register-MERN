edec72e728873eaa2e176747e312a3b3a6e4a06d
Binod-123 <achbinu321@gmail.com>
2024-04-30 00:15:03 +0530

    Useing Routes and Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import About from './Pages/About'
+import Home from './Pages/Home'
 
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/sign-in" element={<Signin/>} />
+      <Route path="/sign-up" element={<Signup/>} />
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
