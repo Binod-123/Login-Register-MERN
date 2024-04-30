f5b3f21e2594baca1208ca79b26f02b9ddbc820c
Binod-123 <achbinu321@gmail.com>
2024-04-30 22:05:53 +0530

    Created Links In Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,18 @@ import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
 import About from './Pages/About'
 import Home from './Pages/Home'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/sign-in" element={<Signin/>} />
       <Route path="/sign-up" element={<Signup/>} />
     </Routes>
-    
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+
+    <div className="bg-slate-200">
+        <div className='felx justify-between items-center'>
+            <div className='font-bold'>
+                <h1>Tech-B</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Sign-in</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
